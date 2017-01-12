0dd989499ff8a00d56b3b0aa7e55a67586bbcf95
Luka Selih <selih.design@gmail.com>
2017-01-12 12:31:43 +0000

    Added plan variables and params to index page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    @basic_plan = Plan.find(1)
+    @intermediate_plan = Plan.find(2)
+    @advanced_plan = Plan.find(3)
   end
   
   def tuts
